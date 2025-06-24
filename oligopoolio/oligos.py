325b35e4fd837c62e3e8135462285e7bfe661bb2
Ariane Mora <ariane.n.mora@gmail.com>
2025-06-24 15:06:55 -0700

    Added in the pdf

--- oligopoolio/oligos.py
+++ oligopoolio/oligos.py
@@ -19,28 +19,23 @@ import Levenshtein
 import numpy as np
 from functools import partial
 import random
-# Check the overlaps by making the plasmid map
 
 
 def create_oligos(seq_df, seq_col, id_col, genbank_file, bsa, min_overlap, max_overlap, optimal_seq_len, min_seq_len, max_seq_len, tm_tolerance, 
                   reverse=False, insert_position=5193):
     rows = []
     for seq, seq_id in seq_df[[seq_col, id_col]].values:
-        oligos = optimize_fragments_for_gene(seq, min_overlap, max_overlap, optimal_seq_len, min_seq_len, max_seq_len, tm_tolerance)
-        # Add in the optimzed sequence
+        oligos, best_cost = optimize_fragments_for_gene(seq, min_overlap, max_overlap, optimal_seq_len, min_seq_len, max_seq_len, tm_tolerance)
         translation_label = f"Insert_{seq_id}"
         record = insert_sequence_with_translation(genbank_file, None, insert_position, seq, translation_label, reverse)
         prev_oligo = None
-        # If a genbank file was provided also just add in the new sequnece
         if len(oligos) > 0:
             for i, oligo in enumerate(oligos):
                 seq = oligo
                 primer_overlap, primer_tm, primer_len, homodimer_tm, hairpin_tm = None, None, None, None, None
                 if prev_oligo:  
-                    # Get the overlap with the previous sequence
                     match = SequenceMatcher(None, prev_oligo, seq).find_longest_match()
                     primer_overlap = prev_oligo[match.a:match.a + match.size]
-                    # Analyze the primer sequence
                     results = check_secondary_structure(primer_overlap)
                     homodimer_tm = results['homodimer']['homodimer_dg']
                     hairpin_tm = results['hairpin']['hairpin_dg']
@@ -49,8 +44,7 @@ def create_oligos(seq_df, seq_col, id_col, genbank_file, bsa, min_overlap, max_o
                 prev_oligo = seq
                 orig_seq = seq
                 strand = 1
-                # Changed this to be the opposite To
-                if bsa == True:
+                if bsa:
                     if i % 2 == 1 and i != 0:
                         seq = str(Seq(seq).reverse_complement())
                         strand = -1
@@ -59,94 +53,78 @@ def create_oligos(seq_df, seq_col, id_col, genbank_file, bsa, min_overlap, max_o
                         seq = str(Seq(seq).reverse_complement())
                         strand = -1
                 oligo_tm = primer3.bindings.calcTm(seq)
-                #if genbank_file:
                 insert_features_from_oligos(record, f"{seq_id}_oligo_{i}", orig_seq, strand, oligo_tm, None)
-                rows.append([seq_id, f"{seq_id}_oligo_{i}", seq, len(seq), oligo_tm, strand, primer_overlap, primer_tm, primer_len, homodimer_tm, hairpin_tm, seq])
+                rows.append([seq_id, f"{seq_id}_oligo_{i}", seq, len(seq), oligo_tm, strand, primer_overlap, primer_tm, primer_len, homodimer_tm, hairpin_tm, seq, best_cost])
             if genbank_file:
                 output_file = genbank_file.replace('.', f'_{seq_id}.')
-                if genbank_file:
-                    record.name = seq_id  # type: ignore 
-                    SeqIO.write(record, f'{output_file}', "genbank")
-    oligo_df = pd.DataFrame(rows, columns=["id", "oligo_id", "oligo_sequence", "oligo_length",  "oligo_tm", "strand","primer_overlap_with_previous", "overlap_tm_5prime", "overlap_length", 
-                                                "overlap_homodimer_dg", "overlap_hairpin_dg", "original_sequence"])
-    # Want to do levenstein distance and make sure there is at least an edit distance of 8 otherwise these are too similar.
-    overlaps = [x for x in oligo_df['primer_overlap_with_previous'].values if x != None]
-
-    min_dist = 100000
-    for i, x in enumerate(overlaps):
-        for j, y in enumerate(overlaps):
-            if i != j:
-                dist = Levenshtein.distance(x, y)
-                if dist < min_dist:
-                    min_dist = dist
-    if min_dist < 8:
-        print(min_dist)
+                record.name = seq_id
+                SeqIO.write(record, f'{output_file}', "genbank")
+    oligo_df = pd.DataFrame(rows, columns=["id", "oligo_id", "oligo_sequence", "oligo_length", "oligo_tm", "strand", "primer_overlap_with_previous", "overlap_tm_5prime", "overlap_length", "overlap_homodimer_dg", "overlap_hairpin_dg", "original_sequence", "best_cost"])
     return oligo_df
-
 
 def optimize_fragments_for_gene(seq, min_overlap, max_overlap, optimal_seq_len, min_seq_len, max_seq_len, tm_tolerance):
     seq_len = len(seq)
     num_fragments = int(math.ceil(seq_len / optimal_seq_len))
-    # Make sure it is even
     if num_fragments % 2 == 1:
         num_fragments += 1
-            
-    # Define bounds for cuts
     num_cuts = num_fragments - 1
-    lower_bounds = np.arange(1, num_cuts + 1) * 2  # avoid first char
-    upper_bounds = np.full(num_cuts, seq_len - 2)
-    
-    # Run PSO
+    num_variables = num_cuts * 2
+
+    cut_lower_bounds = np.arange(1, num_cuts + 1) * 2
+    cut_upper_bounds = np.full(num_cuts, seq_len - 2)
+    olap_lower_bounds = np.full(num_cuts, min_overlap)
+    olap_upper_bounds = np.full(num_cuts, max_overlap)
+
+    lower_bounds = np.concatenate([cut_lower_bounds, olap_lower_bounds])
+    upper_bounds = np.concatenate([cut_upper_bounds, olap_upper_bounds])
+
     bounds = (lower_bounds, upper_bounds)
+
     optimizer = ps.single.GlobalBestPSO(
         n_particles=150,
-        dimensions=num_cuts,
-        options={'c1': 0.5, 'c2': 0.5, 'w': 0.7}, # Balance between explorationa and exploitation
+        dimensions=num_variables,
+        options={'c1': 0.5, 'c2': 0.5, 'w': 0.7},
         bounds=bounds
     )
-    
-    best_cost, best_pos = optimizer.optimize(partial(objective_function, seq_len=seq_len, num_fragments=num_fragments, seq=seq, 
+
+    best_cost, best_pos = optimizer.optimize(partial(objective_function, seq_len=seq_len, num_fragments=num_fragments, seq=seq,
                                                      max_overlap=max_overlap, min_overlap=min_overlap,
                                                      max_seq_len=max_seq_len, min_seq_len=min_seq_len, tm_tolerance=tm_tolerance), iters=100)
-    
-    # Decode result
-    cuts = np.sort(np.round(best_pos).astype(int))
-    this_overlap = int(max_overlap + min_overlap) / 2
-    fragments = get_fragments(cuts, seq_len, num_fragments, this_overlap)
-    fragment_strings = [seq[start:end] for start, end in fragments]
-    
-    return fragment_strings
 
-def get_fragments(cuts, seq_len, num_fragments, overlap_len):
+    cuts = np.sort(np.round(best_pos[:num_cuts]).astype(int))
+    overlaps = np.round(best_pos[num_cuts:]).astype(int)
+    fragments = get_fragments(cuts, seq_len, num_fragments, overlaps)
+    fragment_strings = [seq[start:end] for start, end in fragments]
+
+    return fragment_strings, best_cost
+
+def get_fragments(cuts, seq_len, num_fragments, overlap_lens):
     cuts = np.sort(np.round(cuts).astype(int))
     starts = [0] + list(cuts)
     fragments = []
 
     for i in range(num_fragments):
         start = starts[i]
-        # estimate end based on next start minus overlap
         if i < num_fragments - 1:
             next_start = starts[i + 1]
-            end = next_start + overlap_len
+            end = next_start + overlap_lens[i]
         else:
             end = seq_len
         end = min(end, seq_len)
-        fragments.append((start, end))
+        fragments.append((int(start), int(end)))
     return fragments
-
 
 def objective_function(x, seq_len, num_fragments, seq, tm_optimal=62, min_dist=8, max_overlap=27, min_overlap=18, 
                        min_seq_len=80, max_seq_len=130, tm_tolerance=5):
     penalties = []
     for particle in x:
-        this_overlap = int(max_overlap + min_overlap) / 2
-        fragments = get_fragments(particle, seq_len, num_fragments, this_overlap)
+        cuts = particle[:num_fragments-1]
+        overlaps = particle[num_fragments-1:]
+        fragments = get_fragments(cuts, seq_len, num_fragments, overlaps)
         frag_lengths = [end - start for start, end in fragments]
-        
-        # Overlap check
+
         olap_penalty = 0
-        # Also keep track of the overlaps so that we can select ones which aren't similar
-        overlaps = [] 
+        overlaps_seqs = []
         temp_penalty = 0
         seq_len_penalty = 0
         homodimer_penalty = 0
@@ -154,56 +132,42 @@ def objective_function(x, seq_len, num_fragments, seq, tm_optimal=62, min_dist=8
         for i in range(len(fragments) - 1):
             start1, end1 = fragments[i]
             start2, end2 = fragments[i + 1]
-            overlap = end1 - start2
-            # Check the temperature of the overlap and the dimerization 
-            results = check_secondary_structure(seq[start2: end1])
+            overlap_seq = seq[start2: end1]
+            results = check_secondary_structure(overlap_seq)
             homodimer_tm = results['homodimer']['homodimer_dg']
             hairpin_tm = results['hairpin']['hairpin_dg']
-            
-            # Penalize if Tm deviates from 60
-            primer_tm = primer3.bindings.calcTm(seq[start2: end1])
-            
-            tm_penalty = np.abs(primer_tm - tm_optimal)
-            if overlap < min_overlap or overlap > max_overlap:
-                olap_penalty += 10 * abs(overlap - (min_overlap + max_overlap)/2)
+            primer_tm = primer3.bindings.calcTm(overlap_seq)
 
-            # Have a tolerance for the TM (i.e. is 6C ok?)
-            if tm_penalty > tm_tolerance:
-                temp_penalty += 2*tm_penalty
-            else:
-                temp_penalty += tm_penalty
-            if end1 - start1 > max_seq_len:# or end1 - start1 < min_seq_len:
-                seq_len_penalty += 10*abs(max_seq_len- (end1 - start1))
-            elif end1 - start1 < min_seq_len: 
-                seq_len_penalty += 10*abs(min_seq_len- (end1 - start1))
-            if end2 - start2 > max_seq_len:# or end1 - start1 < min_seq_len:
-                seq_len_penalty += 10*abs(max_seq_len- (end2 - start2))
-            elif end2 - start2 < min_seq_len: 
-                seq_len_penalty += 10*abs(min_seq_len- (end2 - start2)) 
-            if homodimer_tm < -3:
-                # Add an extra penalty if it's outside the range
-                homodimer_penalty += 2*abs(homodimer_tm)
-            else:
-                homodimer_penalty += abs(homodimer_tm)
-            overlap_seq = seq[start2: end1]
+            tm_penalty = np.abs(primer_tm - tm_optimal)
+            if len(overlap_seq) < min_overlap or len(overlap_seq) > max_overlap:
+                olap_penalty += 10 * abs(len(overlap_seq) - (min_overlap + max_overlap)/2)
+            temp_penalty += 2*tm_penalty if tm_penalty > tm_tolerance else tm_penalty
+
+            for frag_len in [end1 - start1, end2 - start2]:
+                if frag_len > max_seq_len:
+                    seq_len_penalty += 10 * abs(max_seq_len - frag_len)
+                elif frag_len < min_seq_len:
+                    seq_len_penalty += 10 * abs(min_seq_len - frag_len)
+
+            homodimer_penalty += 2*abs(homodimer_tm) if homodimer_tm < -3 else abs(homodimer_tm)
             if overlap_seq[:3] in ['AAA', 'TTT', 'CCC', 'GGG'] or overlap_seq[-3:] in ['AAA', 'TTT', 'CCC', 'GGG']:
                 repeat_penalty += 20
-            overlaps.append(seq[start2: end1])
-        
-        # Want to do levenstein distance and make sure there is at least an edit distance of 8 otherwise these are too similar.
-        min_dist = 100000
-        for i, x in enumerate(overlaps):
-            for j, y in enumerate(overlaps):
+            overlaps_seqs.append(overlap_seq)
+
+        min_dist_val = 100000
+        for i, x in enumerate(overlaps_seqs):
+            for j, y in enumerate(overlaps_seqs):
                 if i != j:
                     dist = Levenshtein.distance(x, y)
-                    if dist < min_dist:
-                        min_dist = dist
-        dist_penalty = max_overlap - min_dist
-        if min_dist < min_dist:
+                    if dist < min_dist_val:
+                        min_dist_val = dist
+        dist_penalty = max_overlap - min_dist_val
+        if min_dist_val < min_dist:
             dist_penalty += max_overlap
-        # Variance penalty
+
         mean_len = np.mean(frag_lengths)
         variance = np.sqrt(np.sum([(l - mean_len)**2 for l in frag_lengths]))
+
         penalties.append(variance + olap_penalty + homodimer_penalty + temp_penalty + seq_len_penalty + dist_penalty + repeat_penalty)
     return np.array(penalties)
 
